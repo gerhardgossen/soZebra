80e884a1e31ddd8a739b9e8dbcd550d8d6463e4c
Gerhard Gossen <gerhard.gossen@googlemail.com>
2010-11-04 12:41:53 +0100

    Add Mallet

---
+++ project/build/SoZebra.scala
@@ -0,0 +1,7 @@
+import sbt._
+
+class EmailClassificationProject(info: ProjectInfo) extends DefaultProject(info) {
+    // Mallet dependencies
+    val trove = "trove" % "trove"  % "1.0.2"
+    //val specs = "org.scala-tools.testing" % "specs" % "1.6.5" % "test"
+}
