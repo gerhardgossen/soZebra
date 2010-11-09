e3c145c16ff78b0fff8e9a610d09ee854a137ddc
Gerhard Gossen <gerhard.gossen@googlemail.com>
2010-11-09 13:07:43 +0100

    Add sql to create aggregate views

---
+++ create_view_annotations.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW message_annotations AS
+SELECT messageid, array_agg(annvalue) AS values, array_agg(lineorder) AS linesnumbers
+FROM zoneannotations NATURAL JOIN zonelines
+WHERE errorid = 'n'
+GROUP BY messageid
---
+++ create_view_messages.sql
@@ -0,0 +1,25 @@
+﻿CREATE OR REPLACE VIEW messages AS SELECT *
+FROM bodies
+NATURAL LEFT JOIN 
+  (SELECT messageid, array_agg(email) AS to_email, array_agg(name) AS to_name
+  FROM recipients NATURAL JOIN people 
+  WHERE reciptype = 'to'
+  GROUP BY messageid) AS "to"
+NATURAL LEFT JOIN
+  (SELECT messageid, array_agg(email) AS cc_email, array_agg(name) AS cc_name
+  FROM recipients NATURAL JOIN people 
+  WHERE reciptype = 'cc'
+  GROUP BY messageid) AS cc
+NATURAL LEFT JOIN
+  (SELECT messageid, array_agg(email) AS bcc_email, array_agg(name) AS bcc_name
+  FROM recipients NATURAL JOIN people 
+  WHERE reciptype = 'bcc'
+  GROUP BY messageid) AS bcc
+NATURAL LEFT JOIN
+  (SELECT messageid, headervalue as sender, name AS sendername
+  FROM headers LEFT JOIN people ON (headervalue = email)
+  WHERE headername = 'From') AS sender
+NATURAL LEFT JOIN
+  (SELECT messageid, headervalue as subject
+  FROM headers 
+  WHERE headername = 'Subject') AS subject
