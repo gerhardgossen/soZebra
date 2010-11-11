9d82a8fd998002065c2131ecd8022730d9d8aebd
Gerhard Gossen <gerhard.gossen@googlemail.com>
2010-11-11 14:01:07 +0100

    Adopt Message to actual data, remove javax.mail dependency

--- project/build/SoZebra.scala
+++ project/build/SoZebra.scala
@@ -4,5 +4,4 @@ class EmailClassificationProject(info: ProjectInfo) extends DefaultProject(info)
     // Mallet dependencies
     val trove = "trove" % "trove"  % "1.0.2"
     //val specs = "org.scala-tools.testing" % "specs" % "1.6.5" % "test"
-    val mail = "javax.mail" % "mail" % "1.4"
 }
