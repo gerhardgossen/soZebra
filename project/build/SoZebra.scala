bbb5b73a6f30d3594c4395d4e63900f1a0acb767
Gerhard Gossen <gerhard.gossen@googlemail.com>
2010-11-15 13:53:03 +0100

    Add postgresql dependency

--- project/build/SoZebra.scala
+++ project/build/SoZebra.scala
@@ -4,4 +4,5 @@ class EmailClassificationProject(info: ProjectInfo) extends DefaultProject(info)
     // Mallet dependencies
     val trove = "trove" % "trove"  % "1.0.2"
     //val specs = "org.scala-tools.testing" % "specs" % "1.6.5" % "test"
+    val postgres = "postgresql" % "postgresql" % "8.4-702.jdbc4"
 }
