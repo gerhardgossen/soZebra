ab29b750890a156b33d83e09a7b33478d3f10659
Gerhard Gossen <gerhard.gossen@googlemail.com>
2010-11-15 15:33:37 +0100

    Split message instances into line instances

--- src/main/scala/de/ovgu/findke/gossen/sozebra/Pipeline.scala
+++ src/main/scala/de/ovgu/findke/gossen/sozebra/Pipeline.scala
@@ -1,8 +1,10 @@
 package de.ovgu.findke.gossen.sozebra
 
 import cc.mallet.pipe._
+import cc.mallet.types.{FeatureVectorSequence,Instance,LabelSequence}
 import scala.collection.JavaConversions._
 import de.ovgu.findke.gossen.sozebra.features._
+import java.util.Iterator
 
 object Pipeline {
     def getPipeline: SerialPipes = {
@@ -30,9 +32,51 @@ object Pipeline {
             new FragmentContainsSenderName,
             new PriorFragmentContainsSenderName,
             new FragmentContainsSenderInitials,
-            new FragmentContainsRecipientName
+            new FragmentContainsRecipientName,
+            new SplitSequenceIntoInstances
         )
 
         new SerialPipes(pipes)
+    }
+}
+
+class SplitSequenceIntoInstances extends Pipe {
+    class InstancesIterator(source: Iterator[Instance]) extends Iterator[Instance] {
+        var currentInstance = splitInstance(source.next)
+
+        def remove = throw new UnsupportedOperationException
+
+        def hasNext = currentInstance.hasNext || source.hasNext
+
+        def next = {
+            if (currentInstance hasNext) {
+                currentInstance.next
+            } else if (source hasNext) {
+                currentInstance = splitInstance(source next)
+                currentInstance.next
+            } else {
+                null
+            }
+        }
+
+        private def splitInstance(inst: Instance): Iterator[Instance] = {
+            val data = inst.getData.asInstanceOf[FeatureVectorSequence]
+            val target = inst.getTarget.asInstanceOf[LabelSequence]
+            (data.iterator zip target.iterator) map { case (d, t) => 
+                new Instance(d, t, inst.getName, inst.getSource)
+            }
+        }
+    }
+
+    override def newIteratorFrom(source: Iterator[Instance]): Iterator[Instance] = {
+        if (source hasNext) {
+            new InstancesIterator(source)
+        } else {
+            new Iterator[Instance] {
+                def hasNext = false
+                def next = null
+                def remove = throw new UnsupportedOperationException
+            }
+        }
     }
 }
